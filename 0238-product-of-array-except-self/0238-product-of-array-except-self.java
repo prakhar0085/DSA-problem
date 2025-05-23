50630679f334dbb92103db804d56f55113064425
prakhartiwari0085 <132892214+prakhar0085@users.noreply.github.com>
2025-05-23 12:19:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n]; // Declare and initialize answer array
+
+        for (int i = 0; i < n; i++) {
+            int product = 1;
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    product *= nums[j];
+                }
+            }
+            answer[i] = product;
+        }
+
+        return answer; // Return the whole array
+    }
+}
